d20c407783e8193e4f739e5f33f1146f3a235c53
arsen <holdsena@mail.ru>
2026-03-28 15:48:08 +0300

    Домашнее задание №9 описание: ФИКС

--- src/Lesson9/Animal.java
+++ src/Lesson9/Animal.java
@@ -1,10 +1,6 @@
 package Lesson9;
 
-class Animal {
-    public void eat(String food) {
-        System.out.println("Животное что-то ест");
-    }
-    public void voice() {
-        System.out.println("Животное издает звук");
-    }
+public abstract class Animal {
+    public abstract void eat(String food);
+    public abstract void voice();
 }
