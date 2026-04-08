0209e110a0275d690765f0cd9f0233dd05173016
arsen <holdsena@mail.ru>
2026-04-08 20:11:53 +0300

    Домашнее задание №12 описание: 1. В 1 задание я понял о чём ты, как я говорил я плохо разбераюсь с \b[A-ZА-Я]{2,6}\b но вроде понимаю как это сделать 2. ты писал могу не заморачиваться но я сделал чтобы в дальнейшем в моих программах где надо огромный код, я научился делать так

--- src/HomeWork12/HomeWork12.java
+++ src/HomeWork12/HomeWork12.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Сорри сделал в main но когда ты скажешь мерджить я за мерджу и удалю!
+ */
 public class HomeWork12 {
     public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
