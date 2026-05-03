697e7eb0530e33158bc7f786f2a414f52e435d38
arsen <holdsena@mail.ru>
2026-05-03 16:46:48 +0300

    Домашнее задание №17 описание: 1. ffiix

--- src/HomeWork17/AllLesson.java
+++ src/HomeWork17/AllLesson.java
@@ -1,25 +1,25 @@
 package HomeWork17;
+
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 /**
  * Домашнее задание №17
  * Данный класс содержит комплексное решение задач по теме Data API.
  * Основные темы:
-
  *   - Data/Time API
  *   - Функциональное интерфейсы
  *   - Лямбда выражения
-
  * @author Arsen Shaternik ★
  * @author inspector Nikita Shaternik ★
- * @version New
+ * @version FIXED
  * @see java.util.Date
  * @since 29.04.2026
-
  * Дата и её свойства
  * Чтобы скипнуть дз /skip
  * /skip - Работает только в первой задании, так сделано чтобы было удобно!
@@ -30,28 +30,31 @@ public class AllLesson {
     public static final String PURPLE = "\u001B[35m";
     public static final String BLUE = "\u001B[34m";
     public static final String AQUA = "\u001B[36m";
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        java.time.LocalDate birthday = null;
-        while (birthday == null) {
+        LocalDate birthday;
+        while (true) {
             System.out.println(PURPLE + "Введите дату своего рождения: " + RESET);
             String happyBirthday = scan.nextLine();
-            if(happyBirthday.equalsIgnoreCase("/Skip")) {
+            if (happyBirthday.equalsIgnoreCase("/Skip")) {
                 Help();
                 return;
             }
             try {
-                birthday = java.time.LocalDate.parse(happyBirthday);
-            } catch (java.time.format.DateTimeParseException e) {
+                birthday = LocalDate.parse(happyBirthday);
+                break;
+            } catch (DateTimeParseException e) {
                 System.out.println(RED + "Ошибка!! Только цифры! Пример: 2000-01-01" + RESET);
             }
         }
-        java.time.LocalDate date100 = birthday.plusYears(100);
+        LocalDate date100 = birthday.plusYears(100);
         System.out.println(BLUE + "Дата принята!" + RESET);
         System.out.println(AQUA + "Дата через 100 лет: " + date100 + RESET);
         System.out.println("\n" + "\n" + "\n");
         LessonTwo();
     }
+
     public static void LessonTwo() {
         System.out.println(RED + "Домашнее задание #2" + RESET);
         Predicate<Integer> predicate = n -> n >= 0;
@@ -63,15 +66,21 @@ public class AllLesson {
         }
         LessonThree();
     }
+
     public static void LessonThree() {
         System.out.println(RED + "Домашнее задание #3" + RESET);
         Scanner scan = new Scanner(System.in);
         System.out.println("Какая у тебя сумма BYN?");
-        int balance = scan.nextInt();
-        Function<String, Double> money = s -> Double.parseDouble(s.split(" ")[0]) / 2.82;
-        System.out.println("Переведение в доллары: " + money.apply(balance + " BYN") + "$");
-        LessonFour();
+        try {
+            int balance = scan.nextInt();
+            Function<String, Double> money = s -> Double.parseDouble(s.split(" ")[0]) / 2.82;
+            System.out.println("Переведение в доллары: " + money.apply(balance + " BYN") + "$");
+            LessonFour();
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка! Пользователь ввёл строку!");
+        }
     }
+
     public static void LessonFour() {
         System.out.println(RED + "Домашнее задание #4" + RESET);
         Scanner scan = new Scanner(System.in);
@@ -84,6 +93,7 @@ public class AllLesson {
         money.accept(balance + " BYN");
         LessonFive();
     }
+
     public static void LessonFive() {
         System.out.println(RED + "Домашнее задание #5" + RESET);
         Scanner scan = new Scanner(System.in);
@@ -94,18 +104,17 @@ public class AllLesson {
         };
         System.out.println("Перевёртыш: " + text.get());
     }
+
     public static void Help() {
         Scanner help = new Scanner(System.in);
         System.out.println("Окей давай сделаем чуточку быстрее");
         System.out.println("Напиши какой ты дз хочешь увидеть(2-5)");
         if (help.hasNextInt()) {
             int chose = help.nextInt();
-             if (chose == 2) LessonTwo();
-             else if (chose == 3) LessonThree();
-             else if (chose == 4) LessonFour();
-             else if (chose == 5) LessonFive();
-
+            if (chose == 2) LessonTwo();
+            else if (chose == 3) LessonThree();
+            else if (chose == 4) LessonFour();
+            else if (chose == 5) LessonFive();
         }
     }
 }
-
